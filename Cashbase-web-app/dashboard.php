b9f29b7935d7f706de9990187c39a30b01252ea6
Mauricio <79538739+vulp0@users.noreply.github.com>
2021-03-08 14:42:14 -0600

    Add files via upload

--- Cashbase-web-app/dashboard.php
+++ Cashbase-web-app/dashboard.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<!-- Comentarios abajo -->
 <!--
 Aqui sera la ventana general del usuario, aqui tendria que haber:
 -Una barra que diga algo tipo: Bienvenido [nombre del usuario]
@@ -16,12 +17,72 @@ aumento y lineas rojas para perdida
         <title>Cashbase - Tablero</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        
     </head>
     
     <body>
-        <link rel="stylesheet" href="styles.css">
-        <div>
-            
+        
+        <!-- dashboard sidebar -->
+        <div class="dashboardSidebar">
+            <a href="">Chocoflan</a>
+            <a>Cuchufle</a>
+            <a>Capirotada</a>
+            <a>Ensalada de pollo</a>
         </div>
+        
+        <!-- everything else in the dashboard, the main thing -->
+        <div class="dashboardMain">
+            <div class="dashboardElement">Hamana hamana hamana hamana hamana hamana hamana hamana hamana hamana hamana hamana </div>
+        </div>
+        
+        <!-- scriptssss -->
+        <script src="C:\Users\Equipo\node_modules\chart.js\dist\Chart.js"></script>
+        
+        <script>
+            var lineChart = new Chart(ctx, {
+                type: 'line',
+                data: data,
+                options: options
+            });
+        </script>
+        
+        <!-- sidebar styling -->
+        <style>
+            .dashboardSidebar{
+                height:100%;
+                width:140px;
+                position:fixed;
+                z-index:1;
+                top:0;
+                left:0;
+                background-color: #091933;
+                overflow-x:hidden;
+                padding-top:20px;
+            }
+            
+            .dashboardSidebar a{
+                padding: 6px 8px 6px 16px;
+                text-decoration: none;
+                font-size:20px;
+                color:#818181;
+                display:block;
+            }
+            
+            .dashboardSidebar a:hover{
+                color: #f1f1f1;
+            }
+            
+            .dashboardMain{
+                margin-left:160px;
+                padding: 0px 10px;
+            }
+            
+            .dashboardElement{
+                color:#ffd300;
+            }
+        </style>
+        
+        
     </body>
 </html>
